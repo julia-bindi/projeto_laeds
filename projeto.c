3da1083166ce086a88d1e5c68c041b8d1615c0a1
julia-bindi <juliabindi@gmail.com>
2020-10-23 14:31:57 -0300

    Main

--- projeto.c
+++ projeto.c
@@ -6,73 +6,94 @@ referente ao livro de NIVIO ZIVIANI */
 
 /* ========================================================================= */
 
-typedef int TipoChave;
-
 typedef struct {
-  int Chave;
-  int Tipo;
-  int NumElementos;
+    int Chave;
+    int Tipo;
+    int NumElementos;
 } TipoItem;
 
 typedef struct TipoCelula *TipoApontador;
 
 typedef struct TipoCelula {
-  TipoItem Item;
-  TipoApontador Prox;
+    TipoItem Item;
+    TipoApontador Prox;
 } TipoCelula;
 
 typedef struct {
-  TipoApontador Primeiro, Ultimo;
+    TipoApontador Primeiro, Ultimo;
 } TipoLista;
 
-void FLVazia(TipoLista *Lista)
-{ Lista -> Primeiro = (TipoApontador) malloc(sizeof(TipoCelula));
-  Lista -> Ultimo = Lista -> Primeiro;
-  Lista -> Primeiro -> Prox = NULL;
+void FLVazia(TipoLista *Lista){ 
+    Lista -> Primeiro = (TipoApontador) malloc(sizeof(TipoCelula));
+    Lista -> Ultimo = Lista -> Primeiro;
+    Lista -> Primeiro -> Prox = NULL;
 }
 
-int Vazia(TipoLista Lista)
-{ return (Lista.Primeiro == Lista.Ultimo);
+int Vazia(TipoLista Lista){
+    return (Lista.Primeiro == Lista.Ultimo);
 }
 
-void Insere(TipoItem x, TipoLista *Lista)
-{ Lista -> Ultimo -> Prox = (TipoApontador) malloc(sizeof(TipoCelula));
-  Lista -> Ultimo = Lista -> Ultimo -> Prox;
-  Lista -> Ultimo -> Item = x;
-  Lista -> Ultimo -> Prox = NULL;
+void Insere(TipoItem x, TipoLista *Lista){ 
+    Lista -> Ultimo -> Prox = (TipoApontador) malloc(sizeof(TipoCelula));
+    Lista -> Ultimo = Lista -> Ultimo -> Prox;
+    Lista -> Ultimo -> Item = x;
+    Lista -> Ultimo -> Prox = NULL;
 }
 
-void Retira(TipoApontador p, TipoLista *Lista, TipoItem *Item)
-{ /*  ---   Obs.: o item a ser retirado e  o seguinte ao apontado por  p --- */
-  TipoApontador q;
-  if (Vazia(*Lista) || p == NULL || p -> Prox == NULL) 
-  { printf(" Erro   Lista vazia ou posi  c   a o n  a o existe\n");
-    return;
-  }
-  q = p -> Prox;
-  *Item = q -> Item;
-  p -> Prox = q -> Prox;
-  if (p -> Prox == NULL) Lista -> Ultimo = p;
-  free(q);
+void Retira(TipoApontador p, TipoLista *Lista, TipoItem *Item){
+/*  --- Obs.: o item a ser retirado é o seguinte ao apontado por p --- */
+    TipoApontador q;
+    if (Vazia(*Lista) || p == NULL || p -> Prox == NULL) {
+        printf(" Erro Lista vazia ou posicao nao existe\n");
+        return;
+    }
+    q = p -> Prox;
+    *Item = q -> Item;
+    p -> Prox = q -> Prox;
+    if (p -> Prox == NULL) Lista -> Ultimo = p;
+    free(q);
 }
 
-void Imprime(TipoLista Lista)
-{ TipoApontador Aux;
-  Aux = Lista.Primeiro -> Prox;
-  while (Aux != NULL) 
-    { printf("%d\n", Aux -> Item.Chave);
-      Aux = Aux -> Prox;
+void Imprime(TipoLista Lista){
+    TipoApontador Aux;
+    Aux = Lista.Primeiro -> Prox;
+    while (Aux != NULL){ 
+        printf("%d\n", Aux -> Item.Tipo);
+        Aux = Aux -> Prox;
     }
 }
 
 /* ========================================================================== */
 
-/* código referente ao exercício de tipos de segmentos */
+/* código referente ao exercício de tipos de segmentos adaptado para este problema */
 
 /* ========================================================================== */
 
 void imprimiResultado(int numeroSegmentos, TipoLista lista){
-   
+    int sequencia[] = {1, 3, 2, 3, 1};
+    TipoApontador apontador = lista.Primeiro;
+    int controle = 0;
+    for(int i = 0; i < numeroSegmentos; i++){
+        apontador = apontador->Prox;
+        if(apontador->Item.Tipo == sequencia[controle]){
+            controle++;
+            if(controle == 5){
+                break;
+            }
+        }
+        else if(apontador->Item.Tipo == sequencia[0]){
+            controle = 1;
+        }
+        else{
+            controle = 0;
+        }
+    }
+    if(controle == 5){
+        printf("Resultado: Padrao encontrado.\n");
+    }
+    else{
+        printf("Resultado: Padrao nao encontrado.\n");
+    }
 }
 
 void ordemCrescente(int numeroSegmentos, TipoLista lista){
@@ -82,31 +103,31 @@ void ordemCrescente(int numeroSegmentos, TipoLista lista){
     TipoApontador apontador = lista.Primeiro->Prox;
     for(int i=1;i<numeroSegmentos;i++){
         apontador = apontador->Prox;
-        for(int j=0;j<numerosDistintos;j++){
-            if(apontador->Item.Tipo == ordem[j])
+        for(int controle=0;controle<numerosDistintos;controle++){
+            if(apontador->Item.Tipo == ordem[controle])
                 break;
-            if(j == numerosDistintos-1){
+            if(controle == numerosDistintos-1){
                 numerosDistintos++;
                 ordem[numerosDistintos-1] = apontador->Item.Tipo;
             }
         }
     }
     for(int i=0;i<numerosDistintos;i++){
-        for (int j=0;j<numerosDistintos-1;j++){
-            if(ordem[j] > ordem[j+1]){
-                int aux = ordem[j];
-                ordem[j] = ordem[j+1];
-                ordem[j+1] = aux;
+        for (int controle=0;controle<numerosDistintos-1;controle++){
+            if(ordem[controle] > ordem[controle+1]){
+                int aux = ordem[controle];
+                ordem[controle] = ordem[controle+1];
+                ordem[controle+1] = aux;
             }
         }
-        
     }
     apontador = lista.Primeiro;
     for(int i=0;i<numeroSegmentos;i++){
         apontador = apontador->Prox;
-        for(int j=0;j<numerosDistintos;j++){
-            if(apontador->Item.Tipo == ordem[j]){
-                apontador->Item.Tipo = j+1;
+        for(int controle=0;controle<numerosDistintos;controle++){
+            if(apontador->Item.Tipo == ordem[controle]){
+                apontador->Item.Tipo = controle+1;
+                break;
             }
         }
     }
@@ -118,7 +139,7 @@ void segmentaVetor(int quantidade, int valores[]){
     int numeroSegmentos=1;
     TipoLista lista;
     TipoItem item;
-    TipoApontador apontador;
+    // TipoApontador apontador;
     FLVazia(&lista);
     int valorAtual = valores[0];
     item.Chave = numeroSegmentos;
@@ -133,17 +154,16 @@ void segmentaVetor(int quantidade, int valores[]){
             Insere(item, &lista);
         }
     }
-    apontador = lista.Primeiro;
-    int repeticao = 0;
-    for(int i=0, j=0; i<quantidade;i++){
-        repeticao ++;
-        if(valores[i+1] != apontador->Item.Tipo){
-            j++;
-            apontador->Item.NumElementos = repeticao;
-            apontador->Prox;
-            repeticao = 0;
-        }
-    }
+    // apontador = lista.Primeiro;
+    // int repeticao = 0;
+    // for(int i=0; i<quantidade;i++){
+    //     repeticao ++;
+    //     if(valores[i+1] != apontador->Item.Tipo){
+    //         apontador->Item.NumElementos = repeticao;
+    //         apontador = apontador->Prox;
+    //         repeticao = 0;
+    //     }
+    // }
     ordemCrescente(numeroSegmentos,lista);
 }
 
@@ -164,6 +184,5 @@ int main(){
     for(int i=0; i<quantidade;i++){
         fscanf(file,"%d",&valores[i]);
     }
-    segmentaVetor(quantidade,valores);
-    
+    segmentaVetor(quantidade,valores);   
 }
